b268ded7f7264de87d61667caa440f488fb9949d
1149301853@qq.com <1149301853@qq.com>
2019-02-23 23:04:20 +0800

    homework

---
+++ worksheets/chichengying/SocketServer.java
@@ -0,0 +1,31 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		int port = 7000;
+		ServerSocket serverSocket = new ServerSocket(port);
+		Socket socket = serverSocket.accept();
+		DataInputStream dis = new DataInputStream(new 
+				BufferedInputStream(socket.getInputStream()));
+		DataOutputStream dos = new DataOutputStream(new
+				BufferedOutputStream(socket.getOutputStream()));
+		
+		do{
+			double length = dis.readDouble();
+			System.out.println("服务器端收到的边长数据为：" + length);
+			double result = length * length;
+			dos.writeDouble(result);
+			dos.flush();
+		}while(dis.readInt() != 0);
+		socket.close();
+		serverSocket.close();
+	}
+}
